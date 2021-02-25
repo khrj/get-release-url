e540e648bd80fe822ada5afb5212b62e4fa15489
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-02-26 00:57:23 +0530

    Add tests

--- test.ts
+++ test.ts
@@ -1,17 +1,56 @@
 // See https://deno.land/manual/testing
 import { assertEquals } from "./deps.ts"
+import getRelease, { addProviderMethod } from "./mod.ts"
 
-Deno.test("hello world #1", () => {
-    const x = 1 + 2
-    assertEquals(x, 3)
+Deno.test("Fetch all assets", async () => {
+    const correct = [
+        "https://github.com/KhushrajRathod/KhushrajRathod/releases/download/1.0.0/3.txt",
+        "https://github.com/KhushrajRathod/KhushrajRathod/releases/download/1.0.0/hi-1.txt",
+        "https://github.com/KhushrajRathod/KhushrajRathod/releases/download/1.0.0/hi-2.txt",
+    ].sort()
+
+    const urls = await getRelease({
+        provider: "github",
+        user: "KhushrajRathod",
+        repo: "KhushrajRathod",
+    })
+
+    assertEquals(correct, urls.sort())
 })
 
-Deno.test("hello world #2", () => {
-    const x = 2 + 2
-    assertEquals(x, 4)
+Deno.test("Fetch matched assets", async () => {
+    const correct = [
+        "https://github.com/KhushrajRathod/KhushrajRathod/releases/download/1.0.0/hi-1.txt",
+        "https://github.com/KhushrajRathod/KhushrajRathod/releases/download/1.0.0/hi-2.txt",
+    ].sort()
+
+    const urls = await getRelease({
+        provider: "github",
+        user: "KhushrajRathod",
+        repo: "KhushrajRathod",
+        part: "hi",
+    })
+
+    assertEquals(correct, urls.sort())
 })
 
-Deno.test("hello world #3", () => {
-    const x = 3 + 2
-    assertEquals(x, 5)
+Deno.test("Custom fetch", async () => {
+    addProviderMethod("myProvider", ({ user, repo, part }) => {
+        assertEquals(user, "user")
+        assertEquals(repo, "repo")
+        assertEquals(part, "part")
+
+        return Promise.resolve(["hi"])
+    })
+
+    const correct = ["hi"]
+
+    const urls = await getRelease({
+        provider: "myProvider",
+        user: "user",
+        repo: "repo",
+        part: "part",
+    })
+
+    assertEquals(urls, correct)
 })
